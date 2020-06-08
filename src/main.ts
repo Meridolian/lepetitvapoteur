6448100c4f41e207244faea3e161c0e843b44302
Mathieu Gueribout <mgueribout@gmail.com>
2020-06-09 01:25:53 +0200

    force light mode

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,22 @@ import { platformNativeScriptDynamic } from "nativescript-angular/platform";
 
 import { AppModule } from "./app/app.module";
 
+// FORCE THE LIGHT MODE
+import Theme from "@nativescript/theme";
+Theme.setMode(Theme.Light); // Or Theme.Light
+
+declare namespace androidx {
+    export namespace appcompat {
+     export namespace app {
+      export const AppCompatDelegate: any;
+     }
+    }
+   }
+import { android, on, launchEvent, ApplicationEventData } from '@nativescript/core/application';
+if (android) {
+ on(launchEvent, (args: ApplicationEventData) => {
+  androidx.appcompat.app.AppCompatDelegate.setDefaultNightMode(androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO)
+ });
+}
+
 platformNativeScriptDynamic().bootstrapModule(AppModule);
